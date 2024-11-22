916375fe44d97e0ada937fc22a52811d441f26a4
Ren <Keren@keren.com>
2024-11-22 14:41:19 +0700

    feat: index + migrate

---
+++ database/migrations/2024_11_22_065559_create_transaction_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaction', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('amount',20,2);
+            $table->date('date');
+            $table->string('category');
+            $table->enum('type',['income','expense']);
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaction');
+    }
+};
